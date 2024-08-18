043951655fb09965245543608d1e92ddaeadce52
abdurahman14 <abdu@ngs.ma>
2024-08-18 17:46:48 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Ouvrir cette page à [https://abdurahman14.github.io/dessin_accelerometre/](https://abdurahman14.github.io/dessin_accelerometre/)
+
+## Utiliser comme extension
+
+Ce dépôt peut être ajouté en tant qu'**extension** dans MakeCode.
+
+* ouvrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* cliquez sur **Nouveau projet**
+* cliquez sur **Extensions** dans le menu engrenage
+* recherchez **https://github.com/abdurahman14/dessin_accelerometre** et importez
+
+## Éditer ce projet
+
+Éditer ce dépôt dans MakeCode.
+
+* ouvrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* cliquez sur **Importer** puis cliquez sur **Importer l'URL **
+* collez **https://github.com/abdurahman14/dessin_accelerometre** et cliquez sur importer
+
+#### Métadonnées (utilisées pour la recherche, le rendu)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,73 @@
+input.onButtonPressed(Button.A, function () {
+    if (mode == 0) {
+        if (leds[x + y * 5] == 1) {
+            leds[x + y * 5] = 0
+        } else {
+            leds[x + y * 5] = 1
+        }
+        dessinerLeds()
+        music.play(music.createSoundExpression(WaveShape.Square, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+    } else {
+        music.play(music.createSoundExpression(WaveShape.Square, 500, 500, 255, 0, 200, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+    }
+})
+function accelerationToLedPosition (acceleration: number) {
+    i = Math.round(acceleration / 200 + 2)
+    if (i < 0) {
+        return 0
+    } else if (i > 4) {
+        return 4
+    }
+    return i
+}
+input.onButtonPressed(Button.AB, function () {
+    music.play(music.createSoundExpression(WaveShape.Noise, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+    effacerLeds()
+})
+function effacerLeds () {
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 4; j++) {
+            leds[i + j * 5] = 0
+        }
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    mode += 1
+    if (mode > 1) {
+        mode = 0
+    }
+    music.play(music.createSoundExpression(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+})
+function dessinerLeds () {
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 4; j++) {
+            if (leds[i + j * 5] == 1) {
+                led.plot(i, j)
+            } else {
+                led.unplot(i, j)
+            }
+        }
+    }
+}
+let i = 0
+let y = 0
+let x = 0
+let leds: number[] = []
+let mode = 0
+basic.showIcon(IconNames.Asleep)
+mode = 0
+leds = [0, 24]
+basic.pause(1000)
+effacerLeds()
+basic.forever(function () {
+    if (input.logoIsPressed()) {
+        basic.showString("" + (Math.round(input.compassHeading())))
+    } else {
+        dessinerLeds()
+        x = accelerationToLedPosition(input.acceleration(Dimension.X))
+        y = accelerationToLedPosition(input.acceleration(Dimension.Y))
+        if (mode == 0) {
+            led.toggle(x, y)
+        }
+    }
+})
